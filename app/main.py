7d24b99f38f83654144000558dea5c942cfc6c16
spineda1208 <spineda1208@gmail.com>
2025-03-30 00:14:12 -0400

    fix: make sure skeleton routers are included in the api

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
+from app.routes import images, leaderboard
 
 app = FastAPI()
 
+app.include_router(images.router)
+app.include_router(leaderboard.router)
+
 
 @app.get("/")
-async def hello_world():
-    return {"message": "Hello from hack-psu-2025-backend!"}
+def root():
+    return {"message": "api active"}
